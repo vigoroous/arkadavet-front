3d1bbeef68973b4957d0713120cc958effcf8797
Bogdan Martyshevskiy <vigoroous@gmail.com>
2021-10-13 01:33:49 +0300

    Create simple pagination for products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Layout from '@components/layout'
 import styles from '@styles/home.module.css'
 import useFilters, { FilterActions } from 'hooks/useFilters'
 import { ProductType } from './products/[pid]'
+import { useRouter } from 'next/dist/client/router'
 
 
 // ProductElem
@@ -21,16 +22,93 @@ const ProductElem: FC<ProductType> = ({ id, name, category, imageUrl }) => {
     )
 }
 
+// Paginator
+type PageType = {
+    count: number,
+    previous: string,
+    next: string,
+    results: ProductType[],
+}
+
+// type PaginatorType = {
+//     currentPage: number,
+// }
+type statePage = {
+    currentPage: number,
+}
+
+const initialState: statePage = {
+    currentPage: 1
+}
+
+type PaginatorProps = {
+    count: number,
+    currentPage: number,
+    // setPage: (pageNumber: number) => () => statePage,
+    setPage: React.Dispatch<React.SetStateAction<number>>,
+    limit: number,
+}
+
+// const PaginatorButton: FC<{ id: string, url: string }> = ({ id, url, children }) => {
+//     return (
+        
+//     )
+// }
+
+const Paginator: FC<PaginatorProps> = ({ count, currentPage, setPage, limit }) => {
+    const firstPage =  1
+    const lastPage = Math.ceil(count / limit)
+
+    // if (!currentPage) return <div>Loading...</div>
+    //TODO: handle currentPage < 0
+
+    const PaginatorButton: FC<{ id: number }> = ({ id }) => {
+        return (
+            <button
+                className={`${styles['paginator__button']} ${currentPage === id && styles['paginator__button_active']}`}
+                page-number={id}
+                onClick={()=>setPage(id)}
+            >
+                {id}
+            </button>
+        )
+    }
+
+    const FirstPage = () => currentPage > firstPage ?  <PaginatorButton id={firstPage}/> : null
+    const LastPage = () => currentPage < lastPage ? <PaginatorButton id={lastPage}/> : null
+    const CurrentPage = () => <PaginatorButton id={currentPage}/>
+    const PrevPage = () => currentPage > firstPage + 1 ? <PaginatorButton id={currentPage-1}/> : null
+    const NextPage = () => currentPage < lastPage - 1 ? <PaginatorButton id={currentPage+1}/> : null
+
+    return (
+        <div className={styles['paginator']}>
+                <FirstPage/>
+                <PrevPage/>
+                <CurrentPage/>
+                <NextPage/>
+                <LastPage/>
+        </div>
+    )
+}
+
 // Products
 type ProductsProps = {
     filters: string,
 }
 
 const Products: FC<ProductsProps> = ({ filters }) => {
-    const [data, setData] = useState<ProductType[] | null>(null)
+    const [statePage, setPage] = useState(1)
+    const {query} = useRouter()
+    const [data, setData] = useState<PageType | null>(null)
 
+    const limit = 1
+    const offset = statePage * limit - limit
+
+    console.log(query)
+
+    useEffect(()=>setPage(1), [filters])
     useEffect(() => {
-        fetch(`http://127.0.0.1:8000/api/product/?category=${filters}`, {
+        fetch(`http://127.0.0.1:8000/api/product/?limit=${limit}&offset=${offset}&category=${filters}`, {
             method: 'GET',
         })
             .then(res => {
@@ -39,16 +117,19 @@ const Products: FC<ProductsProps> = ({ filters }) => {
                 else
                     throw new Error("failed to get...")
             })
-            .then((data: ProductType[]) => setData(data))
+            .then((data: PageType) => setData(data))
             .catch(e => console.log(e))
-    }, [filters])
+    }, [limit, offset, filters])
 
     if (!data) return <div>Loading...</div>
     return (
-        <div className={styles['products']}>
-            {data.map(e =>
-                <ProductElem key={e.id} {...e} />)}
-        </div>
+        <>
+            <div className={styles['products']}>
+                {data.results.map(e =>
+                    <ProductElem key={e.id} {...e} />)}
+            </div>
+            <Paginator count={data.count} currentPage={statePage} setPage={setPage} limit={limit}></Paginator>
+        </>
     )
 }
 
@@ -91,7 +172,7 @@ const Controls: FC<ControlProps> = ({ filters, dispatch }) => {
 
 // Home
 const Home: NextPageWithLayout = () => {
-    const [state, dispatch] = useFilters()
+    const [stateFilter, setFilter] = useFilters()
 
     return (
 
@@ -108,8 +189,8 @@ const Home: NextPageWithLayout = () => {
                 </div>
                 <div className={styles['lightbox__image']}></div>
             </div>
-            <Controls dispatch={dispatch} filters={state.filters}/>
-            <Products filters={state.filters} />
+            <Controls filters={stateFilter.filters} dispatch={setFilter} />
+            <Products filters={stateFilter.filters}/>
         </main>
 
     )
