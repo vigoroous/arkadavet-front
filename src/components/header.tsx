d03f04a75a151b67dbb3a9a6de247b96915ee135
Bogdan Martyshevskiy <vigoroous@gmail.com>
2021-10-23 19:13:01 +0300

    Add: mobile menu. Trying to use setTimeout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useRef, useState } from 'react'
 import Link from 'next/link'
 import styles from '@styles/header.module.css'
 
@@ -7,18 +7,51 @@ export type HeaderProps = {
     selected?: 'home' | 'about' | 'contacts'
 }
 
-const Header: FC<HeaderProps> = ({ selected }) =>
-    <header className={styles.header}>
-        <div className={styles.header__wrap}>
-            <div className={styles.header__logo}>
-                <Link href="/"><a className={styles['header__logo-link']}>Аркада-Вет</a></Link>
+const Header: FC<HeaderProps> = ({ selected }) => {
+    const [header, setHeader] = useState(false)
+    const timer = useRef<number | null>(null)
+    const [hide, setHide] = useState(true)
+
+    const openDrawer = () => setHide(false)
+    const hideDrawer = () => {
+        if (timer.current === null) return
+        timer.current = window.setTimeout(() => setHide(true), 500)
+    }
+    const toggleDrawer = () => {
+        setHeader(!header)
+        if (hide)
+            openDrawer()
+        else
+            hideDrawer()
+    }
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.header__wrap_mobile}>
+                <div className={`${styles['header__burger-wrap']}`}>
+                    <div className={`${styles['header__burger']} ${header === true && styles['header__burger_opened']}`} onClick={toggleDrawer}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+                <div className={`${styles['header__logo_mobile']}`}>
+                    <a className={`${styles['header__logo-link_mobile']}`} href="/">Аркада-Вет</a>
+                </div>
             </div>
-            <nav className={styles.header__nav}>
-                <Link href="/"><a className={`${styles['header__nav-link']} ${selected === 'home' && styles['header__nav-link_active']}`}>Каталог</a></Link>
-                <Link href="/about"><a className={`${styles['header__nav-link']} ${selected === 'about' && styles['header__nav-link_active']}`}>О&nbsp;нас</a></Link>
-                <Link href="/contacts"><a className={`${styles['header__nav-link']} ${selected === 'contacts' && styles['header__nav-link_active']}`}>Контакты</a></Link>
-            </nav>
-        </div>
-    </header>
+            <div className={`${styles['header__wrap']} ${header === true && styles['header__wrap_opened']} ${hide === true && styles['header__wrap_closed']}`}>
+                <div className={styles.header__logo}>
+                    <Link href="/"><a className={styles['header__logo-link']}>Аркада-Вет</a></Link>
+                </div>
+                <nav className={`${styles['header__nav']} ${header === true && styles['header__nav_opened']}`}>
+                    <Link href="/"><a className={`${styles['header__nav-link']} ${selected === 'home' && styles['header__nav-link_active']}`}>Каталог</a></Link>
+                    <Link href="/about"><a className={`${styles['header__nav-link']} ${selected === 'about' && styles['header__nav-link_active']}`}>О&nbsp;нас</a></Link>
+                    <Link href="/contacts"><a className={`${styles['header__nav-link']} ${selected === 'contacts' && styles['header__nav-link_active']}`}>Контакты</a></Link>
+                </nav>
+            </div>
+        </header>
+    )
+}
 
 export default Header
