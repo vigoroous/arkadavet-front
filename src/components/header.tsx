48e04e3ed6754788c84492b600b17845744b9cd5
Bogdan Martyshevskiy <vigoroous@gmail.com>
2021-10-24 02:14:38 +0300

    Fix: wrote link correctly

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -25,15 +25,15 @@ const MobileDrawer: FC<MobileDrawerProps> = ({open, selected, delay = 300}) => {
             timer.current = window.setTimeout(() => setActuallyOpen(false), delay)
             return () => window.clearTimeout(timer.current)
         }
-    }, [open])
+    }, [open, delay])
 
     if (!actuallyOpen) return null 
     return(
         <div className={`${styles_mobile['header__nav-wrap']} ${open ? styles_mobile['header__nav-wrap_opened'] : styles_mobile['header__nav-wrap_closed']}`}>
             <nav className={`${styles_mobile['header__nav']} ${open ? styles_mobile['header__nav_opened'] : styles_mobile['header__nav_closed']}`}>
-                <Link href="/"><a className={`${styles_mobile['header__nav-link']} ${selected === 'home' && styles_mobile['header__nav-link_active']}`}>Каталог</a></Link>
-                <Link href="/about"><a className={`${styles_mobile['header__nav-link']} ${selected === 'about' && styles_mobile['header__nav-link_active']}`}>О&nbsp;нас</a></Link>
-                <Link href="/contacts"><a className={`${styles_mobile['header__nav-link']} ${selected === 'contacts' && styles_mobile['header__nav-link_active']}`}>Контакты</a></Link>
+                <Link href="/" passHref><a className={`${styles_mobile['header__nav-link']} ${selected === 'home' && styles_mobile['header__nav-link_active']}`}>Каталог</a></Link>
+                <Link href="/about" passHref><a className={`${styles_mobile['header__nav-link']} ${selected === 'about' && styles_mobile['header__nav-link_active']}`}>О&nbsp;нас</a></Link>
+                <Link href="/contacts" passHref><a className={`${styles_mobile['header__nav-link']} ${selected === 'contacts' && styles_mobile['header__nav-link_active']}`}>Контакты</a></Link>
             </nav>
         </div>
     )
@@ -57,7 +57,7 @@ const MobileHeader: FC<HeaderProps> = ({ selected }) => {
                     </div>
                 </div>
                 <div className={`${styles_mobile['header__logo']}`}>
-                    <a className={`${styles_mobile['header__logo-link']}`} href="/">Аркада-Вет</a>
+                <Link href="/" passHref><a className={`${styles_mobile['header__logo-link']}`}>Аркада-Вет</a></Link>
                 </div>
             </div>
             <MobileDrawer open={open} selected={selected}/>
@@ -71,12 +71,12 @@ const DesktopHeader: FC<HeaderProps> = ({ selected }) => {
         <header className={`${styles['header']}`}>
             <div className={`${styles['header__wrap']}`}>
                 <div className={`${styles['header__logo']}`}>
-                    <Link href="/"><a className={`${styles['header__logo-link']}`}>Аркада-Вет</a></Link>
+                    <Link href="/" passHref><a className={`${styles['header__logo-link']}`}>Аркада-Вет</a></Link>
                 </div>
                 <nav className={`${styles['header__nav']}`}>
-                    <Link href="/"><a className={`${styles['header__nav-link']} ${selected === 'home' && styles['header__nav-link_active']}`}>Каталог</a></Link>
-                    <Link href="/about"><a className={`${styles['header__nav-link']} ${selected === 'about' && styles['header__nav-link_active']}`}>О&nbsp;нас</a></Link>
-                    <Link href="/contacts"><a className={`${styles['header__nav-link']} ${selected === 'contacts' && styles['header__nav-link_active']}`}>Контакты</a></Link>
+                    <Link href="/" passHref><a className={`${styles['header__nav-link']} ${selected === 'home' && styles['header__nav-link_active']}`}>Каталог</a></Link>
+                    <Link href="/about" passHref><a className={`${styles['header__nav-link']} ${selected === 'about' && styles['header__nav-link_active']}`}>О&nbsp;нас</a></Link>
+                    <Link href="/contacts" passHref><a className={`${styles['header__nav-link']} ${selected === 'contacts' && styles['header__nav-link_active']}`}>Контакты</a></Link>
                 </nav>
             </div>
         </header>
