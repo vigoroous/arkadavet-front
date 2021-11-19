420781285e1e0b1a503d93f818c31950ac54b77e
Bogdan Martyshevskiy <vigoroous@gmail.com>
2021-11-20 01:02:30 +0300

    Update next to latest version; Add: favicon, title.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@styles/globals.css'
 
 import type { ReactElement, ReactNode } from 'react'
+import Head from 'next/head'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 
@@ -16,7 +17,14 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page)
 
-  return getLayout(<Component {...pageProps} />)
+  return getLayout(
+    <>
+      <Head>
+        <title>Аркада-вет</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export const API_HOST = process.env.NEXT_PUBLIC_API_HOST ?? 'http://localhost:8000'
